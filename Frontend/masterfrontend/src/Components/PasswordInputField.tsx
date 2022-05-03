baeb909cb8c1ce6148d32ce1f3c6b64a60d33df0
Erik <eribi813@student.liu.se>
2022-05-03 18:48:18 +0200

    "Refactored code"

--- Frontend/masterfrontend/src/Components/PasswordInputField.tsx
+++ Frontend/masterfrontend/src/Components/PasswordInputField.tsx
@@ -1,66 +1,29 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
+
 
 import {Colors} from '../Assets/Colors';
-import FormHelperText from '@mui/material/FormHelperText';
+
 
 interface Props {
     id: string;
-    sx: any;
+    handleChange: any;
+    handleClickShowPassword: any;
+    values: any;
 }
 
-export const PasswordInputField: React.FC<Props> = ({id, sx}) => {
-    const [values, setValues] = useState({
-        password: '',
-        showPassword: false,
-      });
-      
-    const [errorMessage, seterrorMessage] = useState("");
-    const [validLength, setValidLength] = useState(false);
-    const [hasNumber, setHasNumber] = useState(false);
-    const [upperCase, setUpperCase] = useState(false);
-    const [lowerCase, setLowerCase] = useState(false);
-    const requiredLength = 8;
-
-    const handleClickShowPassword = () => {
-        setValues({
-        ...values,
-        showPassword: !values.showPassword,
-        });
-    };
+export const PasswordInputField: React.FC<Props> = ({id, handleChange, handleClickShowPassword, values}) => {
 
     const handleMouseDownPassword = (event: { preventDefault: () => void; }) => {
         event.preventDefault();
     };
     
-    const handleChange = () => (event: { target: { value: any; }; }) => {
-        setValues({...values, password: event.target.value});
-        seterrorMessage("WRONG");
-    };
-
-    useEffect(() => {
-        setValidLength(values.password.length >= requiredLength ? true : false);
-        setUpperCase(values.password.toLowerCase() !== values.password);
-        setLowerCase(values.password.toUpperCase() !== values.password);
-        setHasNumber(/\d/.test(values.password));  
-      }, [values, requiredLength]);
-     
     return (
-        <FormControl 
-            sx={sx}
-            variant="outlined"
-        >
-            <InputLabel htmlFor={id}>
-            Password
-            </InputLabel>
-
-            <OutlinedInput
+        <OutlinedInput
                 id={id}
                 type={values.showPassword ? 'text' : 'password'}
                 value={values.password}
@@ -79,8 +42,6 @@ export const PasswordInputField: React.FC<Props> = ({id, sx}) => {
                 </InputAdornment>
                 }
                 label="Password"
-            />
-            <FormHelperText error id="error-text-password">{errorMessage}</FormHelperText>
-        </FormControl>      
+            />    
     );
 };
--- Frontend/masterfrontend/src/Screens/RegistrationPage.tsx
+++ Frontend/masterfrontend/src/Screens/RegistrationPage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Container, Paper, Button} from '@mui/material';
+import { useState, useEffect } from 'react';
+import {Container, Paper, Button, FormHelperText} from '@mui/material';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import { PasswordInputField } from '../Components/PasswordInputField';
@@ -7,10 +7,57 @@ import { UsernameInputField } from '../Components/EmailInputField';
 
 import {Colors} from '../Assets/Colors';
 
-export const RegistrationPage: React.FC = () => {    
+export const RegistrationPage: React.FC = () => {
+    
+    // Hooks used for password checks
+    const [errorMessage, seterrorMessage] = useState("");
+
+    const [values, setValues] = useState({
+        password: '',
+        showPassword: false,
+      });
+
+    // Hooks used for email checks
+    
+    // Constants
     const passwordFieldId = "outlined-adornment-password";
     const emailFieldId = "outlined-email";
-    
+    const requiredLength = 8;
+
+    const handleClickShowPassword = () => {
+        setValues({
+        ...values,
+        showPassword: !values.showPassword,
+        });
+    };
+
+    const isPasswordOk = (inputtedPassword: string) => {
+        const isLengthOk = inputtedPassword.length >= requiredLength ? true : false;
+        if (!isLengthOk) return false;
+
+        const containsNumber = /\d/.test(inputtedPassword);
+        if (!containsNumber) return false;
+
+        const containsUpperLetter = inputtedPassword.toLowerCase() !== inputtedPassword;
+        if (!containsUpperLetter) return false;
+
+        const containsLowerLetter = inputtedPassword.toUpperCase() !== inputtedPassword;
+        if (!containsLowerLetter) return false;
+
+        return true;
+    }
+
+    const handleChange = () => (event: { target: { value: any; }; }) => {
+        if(isPasswordOk(event.target.value)) {
+            setValues({...values, password: event.target.value});
+            seterrorMessage("")
+        }
+        else {
+            setValues({...values, password: event.target.value});
+            seterrorMessage("Password must have more than 7 characters, capital letter and a number")
+        }
+    };
+
     return (
     <Container
         sx={{flex: 1, display: 'flex', flexDirection: 'column',
@@ -34,12 +81,22 @@ export const RegistrationPage: React.FC = () => {
                 <UsernameInputField id={emailFieldId}/>
             </FormControl>
 
-
-            <PasswordInputField 
-                id={passwordFieldId} 
-                sx={{m: 1, width: '80vw',
-                marginBottom: '2vh', maxWidth: '400px'}}
-            />
+            <FormControl 
+                sx={{m: 1, width: '80vw', marginTop: '1vh',
+                marginBottom: '1vh', maxWidth: '400px'}}
+                variant="outlined"
+            >
+                <InputLabel htmlFor={passwordFieldId}>
+                    Password
+                </InputLabel>
+                <PasswordInputField 
+                    id={passwordFieldId}
+                    handleChange={handleChange}
+                    handleClickShowPassword={handleClickShowPassword}
+                    values={values}
+                />
+                <FormHelperText error id="error-text-password">{errorMessage}</FormHelperText>
+            </FormControl>
 
             <Button 
                 variant="contained"

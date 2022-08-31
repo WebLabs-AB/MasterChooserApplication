f290afbaa42073896c726cecb02e83477da446e0
Erik <eribi813@student.liu.se>
2022-08-31 21:12:13 +0200

    Started working on e2e backend tests.
    See title.

--- Backend/nest-graphql-backend/test/app.e2e-spec.ts
+++ Backend/nest-graphql-backend/test/app.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('GraphQL AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,24 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  const gql = '/graphql';
+
+  describe(gql, () => {
+    describe('student', () => {
+      it('should create a startingYear', () => {
+        return request(app.getHttpServer())
+          .post(gql)
+          .send({
+            query:
+              'mutation {createNewStartingYear(createStartingYearInput: { startingYear: 2019}) {startingYear}}',
+          })
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.data.createNewStartingYear).toEqual({
+              startingYear: 2019,
+            });
+          });
+      });
+    });
   });
 });

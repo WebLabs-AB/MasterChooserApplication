6e92be9ce8df2cd28ff320d4da3165eeb75b4ccd
Erik <eribi813@student.liu.se>
2022-11-10 19:45:08 +0100

    "Fixed main-area unit test base"

--- Backend/nest-graphql-backend/src/routers/main-area/main-area.service.spec.ts
+++ Backend/nest-graphql-backend/src/routers/main-area/main-area.service.spec.ts
@@ -1,18 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MainArea } from 'src/entities/NormalTypes/MainArea.entity';
+import { Repository } from 'typeorm';
 import { MainAreaService } from './main-area.service';
 
+type MockType<T> = {
+  [P in keyof T]?: jest.Mock<{}>;
+};
+
+let mainareaService: MainAreaService;
+
+const mainAreaRepository: MockType<Repository<MainArea>> = {
+  save: jest.fn(),
+  findOne: jest.fn(),
+  find: jest.fn(),
+  create: jest.fn(),
+};
+
+beforeAll(async () => {
+  const module: TestingModule = await Test.createTestingModule({
+    providers: [
+      MainAreaService,
+      {
+        provide: getRepositoryToken(MainArea),
+        useValue: mainAreaRepository,
+      },
+    ],
+  }).compile();
+
+  mainareaService = module.get(MainAreaService);
+});
+
 describe('MainAreaService', () => {
-  let service: MainAreaService;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MainAreaService],
-    }).compile();
-
-    service = module.get<MainAreaService>(MainAreaService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  test('should be defined', () => {
+    expect(mainareaService).toBeDefined();
   });
 });

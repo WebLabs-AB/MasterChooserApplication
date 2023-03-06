1640617920deffbab48a93e11c1e2e6ccd28ab75
Birger9 <erikbirgersson98@gmail.com>
2023-03-06 15:33:00 +0100

    "Add unit tests for general requirements entity" (#59)
    Co-authored-by: Erik <eribi813@student.liu.se>

--- Backend/nest-graphql-backend/src/routers/education/education.service.spec.ts
+++ Backend/nest-graphql-backend/src/routers/education/education.service.spec.ts
@@ -1,6 +1,6 @@
-import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserInputError } from 'apollo-server-express';
 
 // Own files.
 import { Education } from 'src/entities/NormalTypes/Education.entity';
@@ -108,7 +108,7 @@ describe('Test createEducation func', () => {
         symbol: 'D',
         universityName: 'Chalmers',
       }),
-    ).rejects.toThrowError(BadRequestException);
+    ).rejects.toThrowError(UserInputError);
   });
 });
 

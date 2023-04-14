7d0618466d2961852b7ebacdf794b008710e53dd
Lucas Granberg <lucgr483@student.liu.se>
2023-04-14 11:24:27 +0200

    More dynamic elements

--- Frontend/masterfrontend/src/Screens/CreateMasterPage.tsx
+++ Frontend/masterfrontend/src/Screens/CreateMasterPage.tsx
@@ -6,9 +6,31 @@ import Stack from "@mui/material/Stack";
 import { CourseTable } from "../Components/CourseTable";
 import { ProfileReq } from "../Components/ProfileReq";
 import { GeneralReq } from "../Components/GeneralReq";
+import { InputLabel, MenuItem, Select } from "@mui/material";
 
 export const CreateMasterPage: React.FC = () => {
   const tabs = ["Create master schema", "See your master schema", "Profile"];
+  const specialisations = [
+    "All",
+    "AI och maskininlärning",
+    "Industriell ekonomi",
+    "International Software Engineering",
+    "Medicinsk informatik",
+    "Programmering och algoritmer",
+    "Spelprogrammering",
+    "Storskalig mjukvaruutveckling",
+    "Säkra system",
+  ];
+  const currentSpec = 6;
+  const fields = [
+    "All",
+    "Computer Science",
+    "Computer Science and Engineering",
+    "Information Technology",
+  ];
+  const currentField = 0;
+  const date = "Termin 7 HT 2022";
+
   return (
     <Container>
       <Tabs value={0}>
@@ -18,29 +40,30 @@ export const CreateMasterPage: React.FC = () => {
       </Tabs>
       <Stack direction="row">
         <Stack direction="column">
-          <p>Master</p>
-          <select>
-            <option>AI och maskininlärning</option>
-            <option>Industriell ekonomi</option>
-            <option>International Software Engineering</option>
-            <option>Medicinsk informatik</option>
-            <option>Programmering och algoritmer</option>
-            <option>Spelprogrammering</option>
-            <option>Storskalig mjukvaruutveckling</option>
-            <option>Säkra system</option>
-          </select>
+          <InputLabel id="spec-label">Specialisation</InputLabel>
+          <Select
+            labelId="spec-label"
+            label="Specialisation"
+            value={currentSpec}
+          >
+            {specialisations.map((e, i) => (
+              <MenuItem value={i}>{e}</MenuItem>
+            ))}
+          </Select>
         </Stack>
         <Stack direction="column">
-          <p>Expertise Area</p>
-          <select>
-            <option>All</option>
-          </select>
+          <InputLabel>Field of study</InputLabel>
+          <Select label="Field of study" value={currentField}>
+            {fields.map((e, i) => (
+              <MenuItem value={i}>{e}</MenuItem>
+            ))}
+          </Select>
         </Stack>
       </Stack>
       <Stack direction="row">
         <Stack direction="column">
-          <h2>Termin 7 HT 2022</h2>
-          <h3>Inriktning: Spelprogrammering</h3>
+          <h2>{date}</h2>
+          <h3>Specialisation: {specialisations[currentSpec]}</h3>
           <CourseTable />
         </Stack>
         <Stack direction="column">

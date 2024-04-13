d795783293a9e9707450c88aa808f90d081ac8e4
Erik <eribi813@student.liu.se>
2024-04-13 12:44:38 +0200

    "Update app.module.ts with new modules"

--- Backend/nest-graphql-backend/src/app.module.ts
+++ Backend/nest-graphql-backend/src/app.module.ts
@@ -8,7 +8,29 @@ import { ConfigService } from '@nestjs/config';
 
 // Own files
 import { environment } from './environments/environment';
-import entities from './entities';
+import entities, { CoursePeriod } from './entities';
+import { StudentModule } from './routers/student/student.module';
+import { UniversityModule } from './routers/university/university.module';
+import { EducationModule } from './routers/education/education.module';
+import { StartingYearModule } from './routers/starting-year/starting-year.module';
+import { TeacherModule } from './routers/teacher/teacher.module';
+import { MainAreaModule } from './routers/main-area/main-area.module';
+import { PeriodModule } from './routers/period/period.module';
+import { CourseModule } from './routers/course/course.module';
+import { AuthModule } from './common/services/auth.module';
+import { AdminModule } from './routers/admin/admin.module';
+import { CourseMainAreaModule } from './routers/course-main-area/course-main-area.module';
+import { CoursePeriodModule } from './routers/course-period/course-period.module';
+import { CourseStartingYearModule } from './routers/course-starting-year/course-starting-year.module';
+import { EducationCourseModule } from './routers/education-course/education-course.module';
+import { EducationMainAreaModule } from './routers/education-main-area/education-main-area.module';
+import { MasterProfileModule } from './routers/master-profile/master-profile.module';
+import { MasterProfileCourseOptionalModule } from './routers/master-profile-course-optional/master-profile-course-optional.module';
+import { MasterProfileCourseRequiredModule } from './routers/master-profile-course-required/master-profile-course-required.module';
+import { MasterSchemaModule } from './routers/master-schema/master-schema.module';
+import { MasterSchemaCourseModule } from './routers/master-schema-course/master-schema-course.module';
+import { PackageModule } from './routers/package/package.module';
+import { PackageCourseModule } from './routers/package-course/package-course.module';
 
 @Module({
   imports: [
@@ -63,6 +85,31 @@ import entities from './entities';
       },
       inject: [ConfigService],
     }),
+    StudentModule,
+    UniversityModule,
+    EducationModule,
+    StartingYearModule,
+    TeacherModule,
+    MainAreaModule,
+    PeriodModule,
+    CourseModule,
+    EducationModule,
+    AdminModule,
+    CourseMainAreaModule,
+    CoursePeriodModule,
+    CourseStartingYearModule,
+    EducationCourseModule,
+    EducationMainAreaModule,
+    MasterProfileModule,
+    MasterProfileCourseOptionalModule,
+    MasterProfileCourseRequiredModule,
+    MasterSchemaModule,
+    MasterSchemaCourseModule,
+    PackageModule,
+    PackageCourseModule,
+    StudentModule,
+    UniversityModule,
+    AuthModule,
   ],
   controllers: [],
 })

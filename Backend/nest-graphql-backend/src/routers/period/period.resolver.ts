d679b4988e1bd9c09188aacdcb9239d7ca07efc5
oliolo <oliver_olofsson@hotmail.se>
2024-04-17 16:35:53 +0200

    Implement findAll, create, remove, findOne in Period

--- Backend/nest-graphql-backend/src/routers/period/period.resolver.ts
+++ Backend/nest-graphql-backend/src/routers/period/period.resolver.ts
@@ -1,9 +1,43 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { PeriodService } from './period.service';
+import { Period } from 'src/entities';
+import { CreatePeriodInput } from 'src/inputTypes/create-period.input';
+import { RemovePeriodInput } from 'src/inputTypes/create-period.input';
 
 @Resolver()
 export class PeriodResolver {
-  @Query(() => String)
-  sayHello(): string {
-    return 'Hello World!';
+  constructor(private periodService: PeriodService) {}
+
+  // Returns all CourseMainArea obejcts from database in a list.
+  @Query((returns) => [Period])
+  async period(): Promise<Period[]> {
+    return this.periodService.findAll();
+  }
+
+  // Finds a period from the database.
+  @Query((returns) => Period)
+  async findOnePeriod(
+    @Args('findOnePeriod')
+    createPeriodInput: CreatePeriodInput,
+  ): Promise<Period> {
+    return this.periodService.findOnePeriod(createPeriodInput);
+  }
+
+  // Creates a new CourseMainArea object for the database
+  @Mutation((returns) => Period)
+  async createPeriod(
+    @Args('createPeriodInput')
+    createPeriodInput: CreatePeriodInput,
+  ): Promise<Period> {
+    return this.periodService.createPeriod(createPeriodInput);
+  }
+
+  // Removes a CourseMainArea object for the database
+  @Mutation((returns) => Period)
+  async removePeriod(
+    @Args('removePeriodInput')
+    removePeriodInput: RemovePeriodInput,
+  ): Promise<Period> {
+    return this.periodService.removePeriod(removePeriodInput);
   }
 }
--- Backend/nest-graphql-backend/src/routers/period/period.service.ts
+++ Backend/nest-graphql-backend/src/routers/period/period.service.ts
@@ -1,4 +1,99 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Period } from 'src/entities';
+import { Repository } from 'typeorm';
+import { CreatePeriodInput } from 'src/inputTypes/create/create-period.input';
+import { RemovePeriodInput } from 'src/inputTypes/remove/remove-period.input';
+import { UpdatePeriodInput } from 'src/inputTypes/update/update-period.input';
 
 @Injectable()
-export class PeriodService {}
+export class PeriodService {
+  constructor(
+    @InjectRepository(Period)
+    private periodRepository: Repository<Period>,
+  ) {}
+
+  /**
+   * Description: Finds all Period objects in the database
+   * Input: Null
+   * @returns All Period objects
+   */
+  async findAll(): Promise<Period[]> {
+    return this.periodRepository.find(); // SELECT * FROM coursemainareas;
+  }
+
+  /**
+   * Description: Creates a new period entity in the database.
+   * Input: value
+   * @param createPeriodInput An object containing the information needed to
+   * create a new period.
+   * @returns A Promise that resolves to the created period entity.
+   */
+  async createPeriod(createPeriodInput: CreatePeriodInput): Promise<Period> {
+    if (await this.doesPeriodExists(createPeriodInput.value)) {
+      throw new HttpException('Period already exists', HttpStatus.CONFLICT);
+    }
+
+    const newPeriod = this.periodRepository.create(createPeriodInput);
+    return this.periodRepository.save(newPeriod);
+  }
+
+  /**
+   * Description: Finds a period entity in the database based on the provided input.
+   * @param updatePeriodInput An object containing the value of the period to find.
+   * @returns A Promise that resolves to the found period entity, if it exists.
+   */
+  async findOnePeriod(updatePeriodInput: UpdatePeriodInput): Promise<Period> {
+    // Check if Period exists in the database
+    return this.periodRepository.findOne({
+      where: { value: updatePeriodInput.value },
+    });
+  }
+
+  /**
+   * Description: Removes a period entity from the database.
+   * @param removePeriodInput An object containing the value of the period to be removed.
+   * @returns A Promise that resolves to the removed period entity.
+   * @throws HttpException if the period does not exist in the database.
+   */
+  async removePeriod(removePeriodInput: RemovePeriodInput): Promise<Period> {
+    // Check if Period exists in the database
+    const existingPeriod = await this.periodRepository.findOne({
+      where: {
+        value: removePeriodInput.value,
+      },
+    });
+
+    if (!existingPeriod) {
+      throw new HttpException('Period do not exists', HttpStatus.NOT_FOUND);
+    }
+
+    // If Period exists, delete it
+    return this.periodRepository.remove(existingPeriod);
+  }
+
+  /**
+   * Description: ?
+   * Input: ?
+   * @param updatePeriodInput ?
+   * @returns The new updated Period.
+   */
+  async updatePeriod(updatePeriodInput: UpdatePeriodInput): Promise<Period[]> {}
+
+  /**
+   * Description: Checks if a period with the specified value exists in the database.
+   * @param value The value of the period to check for existence.
+   * @returns A Promise that resolves to a boolean indicating whether the period exists.
+   */
+  async doesPeriodExists(value: number): Promise<boolean> {
+    const period = await this.periodRepository.findOne({
+      where: { value: value },
+    });
+
+    if (!period) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}

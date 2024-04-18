145129e4af4116b02654b681849736dbf1380c69
oliolo <oliver_olofsson@hotmail.se>
2024-04-18 17:38:08 +0200

    Implement all router functions in MainArea

--- Backend/nest-graphql-backend/src/routers/main-area/main-area.resolver.ts
+++ Backend/nest-graphql-backend/src/routers/main-area/main-area.resolver.ts
@@ -1,9 +1,43 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { MainAreaService } from './main-area.service';
+import { MainArea } from 'src/entities';
+import { CreateMainAreaInput } from 'src/inputTypes/create/create-mainArea.input';
+import { RemoveMainAreaInput } from 'src/inputTypes/remove/remove-mainArea.input';
 
 @Resolver()
 export class MainAreaResolver {
-  @Query(() => String)
-  sayHello(): string {
-    return 'Hello World!';
+  constructor(private mainAreaService: MainAreaService) {}
+
+  // Returns all CourseMainArea obejcts from database in a list.
+  @Query((returns) => [MainArea])
+  async mainArea(): Promise<MainArea[]> {
+    return this.mainAreaService.findAll();
+  }
+
+  // Finds a mainArea from the database.
+  @Query((returns) => MainArea)
+  async findOneMainArea(
+    @Args('findOneMainArea')
+    createMainAreaInput: CreateMainAreaInput,
+  ): Promise<MainArea> {
+    return this.mainAreaService.findOneMainArea(createMainAreaInput);
+  }
+
+  // Creates a new CourseMainArea object for the database
+  @Mutation((returns) => MainArea)
+  async createMainArea(
+    @Args('createMainAreaInput')
+    createMainAreaInput: CreateMainAreaInput,
+  ): Promise<MainArea> {
+    return this.mainAreaService.createMainArea(createMainAreaInput);
+  }
+
+  // Removes a CourseMainArea object for the database
+  @Mutation((returns) => MainArea)
+  async removeMainArea(
+    @Args('removeMainAreaInput')
+    removeMainAreaInput: RemoveMainAreaInput,
+  ): Promise<MainArea> {
+    return this.mainAreaService.removeMainArea(removeMainAreaInput);
   }
 }
--- Backend/nest-graphql-backend/src/routers/main-area/main-area.service.ts
+++ Backend/nest-graphql-backend/src/routers/main-area/main-area.service.ts
@@ -1,4 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MainArea } from 'src/entities';
+import { Repository } from 'typeorm';
+import { CreateMainAreaInput } from 'src/inputTypes/create/create-mainArea.input';
+import { RemoveMainAreaInput } from 'src/inputTypes/remove/remove-mainArea.input';
+import { UpdateMainAreaInput } from 'src/inputTypes/update/update-mainArea.input';
 
 @Injectable()
-export class MainAreaService {}
+export class MainAreaService {
+  constructor(
+    @InjectRepository(MainArea)
+    private mainAreaRepository: Repository<MainArea>,
+  ) {}
+
+  /**
+   * Description: Finds all MainArea objects in the database
+   * Input: Null
+   * @returns All MainArea objects
+   */
+  async findAll(): Promise<MainArea[]> {
+    return this.mainAreaRepository.find(); // SELECT * FROM coursemainareas;
+  }
+
+  /**
+   * Description: Creates a new mainArea entity in the database.
+   * Input: value
+   * @param createMainAreaInput An object containing the information needed to
+   * create a new mainArea.
+   * @returns A Promise that resolves to the created mainArea entity.
+   */
+  async createMainArea(
+    createMainAreaInput: CreateMainAreaInput,
+  ): Promise<MainArea> {
+    if (await this.doesMainAreaExists(createMainAreaInput.name)) {
+      throw new HttpException('Main area already exists', HttpStatus.CONFLICT);
+    }
+
+    const newMainArea = this.mainAreaRepository.create(createMainAreaInput);
+    return this.mainAreaRepository.save(newMainArea);
+  }
+
+  /**
+   * Description: Finds a mainArea entity in the database based on the provided input.
+   * @param updateMainAreaInput An object containing the value of the mainArea to find.
+   * @returns A Promise that resolves to the found mainArea entity, if it exists.
+   */
+  async findOneMainArea(
+    updateMainAreaInput: UpdateMainAreaInput,
+  ): Promise<MainArea> {
+    // Check if MainArea exists in the database
+    return this.mainAreaRepository.findOne({
+      where: { name: updateMainAreaInput.name },
+    });
+  }
+
+  /**
+   * Description: Removes a mainArea entity from the database.
+   * @param removeMainAreaInput An object containing the value of the mainArea to be removed.
+   * @returns A Promise that resolves to the removed mainArea entity.
+   * @throws HttpException if the mainArea does not exist in the database.
+   */
+  async removeMainArea(
+    removeMainAreaInput: RemoveMainAreaInput,
+  ): Promise<MainArea> {
+    // Check if MainArea exists in the database
+    const existingMainArea = await this.mainAreaRepository.findOne({
+      where: {
+        name: removeMainAreaInput.name,
+      },
+    });
+
+    if (!existingMainArea) {
+      throw new HttpException('Main area do not exists', HttpStatus.NOT_FOUND);
+    }
+
+    // If MainArea exists, delete it
+    return this.mainAreaRepository.remove(existingMainArea);
+  }
+
+  /**
+   * Description: Checks if a mainArea with the specified value exists in the database.
+   * @param name The name of the mainArea to check for existence.
+   * @returns A Promise that resolves to a boolean indicating whether the mainArea exists.
+   */
+  async doesMainAreaExists(name: string): Promise<boolean> {
+    const mainArea = await this.mainAreaRepository.findOne({
+      where: { name: name },
+    });
+
+    if (!mainArea) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}

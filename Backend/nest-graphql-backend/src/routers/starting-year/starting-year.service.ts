2c5beb8e3feeb14d11ae3957e45b7c4a41590ac8
oliolo <oliver_olofsson@hotmail.se>
2024-04-24 17:02:27 +0200

    Working on router functions in StartingYear

--- Backend/nest-graphql-backend/src/routers/starting-year/starting-year.service.ts
+++ Backend/nest-graphql-backend/src/routers/starting-year/starting-year.service.ts
@@ -1,4 +1,107 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { StartingYear } from 'src/entities';
+import { CreateStartingYearInput } from 'src/inputTypes/create/create-startingYear.input';
+import { RemoveStartingYearInput } from 'src/inputTypes/remove/remove-startingYear.input';
+import { UpdateStartingYearInput } from 'src/inputTypes/update/update-startingYear.input';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class StartingYearService {}
+export class StartingYearService {
+  constructor(
+    @InjectRepository(StartingYear)
+    private startingYearRepository: Repository<StartingYear>,
+  ) {}
+
+  /**
+   * Description: Finds all StartingYear objects in the database
+   * Input: Null
+   * @returns All StartingYear objects
+   */
+  async findAll(): Promise<StartingYear[]> {
+    return this.startingYearRepository.find(); // SELECT * FROM coursemainareas;
+  }
+
+  /**
+   * Description: Creates a new startingYear entity in the database.
+   * Input: startingYear
+   * @param createStartingYearInput An object containing the information needed to
+   * create a new startingYear.
+   * @returns A Promise that resolves to the created startingYear entity.
+   */
+  async createStartingYear(
+    createStartingYearInput: CreateStartingYearInput,
+  ): Promise<StartingYear> {
+    if (
+      await this.doesStartingYearExists(createStartingYearInput.startingYear)
+    ) {
+      throw new HttpException(
+        'Starting year already exists',
+        HttpStatus.CONFLICT,
+      );
+    }
+
+    const newStartingYear = this.startingYearRepository.create(
+      createStartingYearInput,
+    );
+    return this.startingYearRepository.save(newStartingYear);
+  }
+
+  /**
+   * Description: Finds a startingYear entity in the database based on the provided input.
+   * @param updateStartingYearInput An object containing the value of the startingYear to find.
+   * @returns A Promise that resolves to the found startingYear entity, if it exists.
+   */
+  async findOneStartingYear(
+    updateStartingYearInput: UpdateStartingYearInput,
+  ): Promise<StartingYear> {
+    // Check if StartingYear exists in the database
+    return this.startingYearRepository.findOne({
+      where: { startingYear: updateStartingYearInput.startingYear },
+    });
+  }
+
+  /**
+   * Description: Removes a startingYear entity from the database.
+   * @param removeStartingYearInput An object containing the value of the startingYear to be removed.
+   * @returns A Promise that resolves to the removed startingYear entity.
+   * @throws HttpException if the startingYear does not exist in the database.
+   */
+  async removeStartingYear(
+    removeStartingYearInput: RemoveStartingYearInput,
+  ): Promise<StartingYear> {
+    // Check if StartingYear exists in the database
+    const existingStartingYear = await this.startingYearRepository.findOne({
+      where: {
+        startingYear: removeStartingYearInput.startingYear,
+      },
+    });
+
+    if (!existingStartingYear) {
+      throw new HttpException(
+        'Starting year do not exists',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    // If StartingYear exists, delete it
+    return this.startingYearRepository.remove(existingStartingYear);
+  }
+
+  /**
+   * Description: Checks if a startingYear with the specified value exists in the database.
+   * @param startingYear The value of the startingYear to check for existence.
+   * @returns A Promise that resolves to a boolean indicating whether the startingYear exists.
+   */
+  async doesStartingYearExists(startingYear: number): Promise<boolean> {
+    const startYear = await this.startingYearRepository.findOne({
+      where: { startingYear: startingYear },
+    });
+
+    if (!startYear) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}

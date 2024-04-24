bd637c08a2ededd8e8e59c96be1d92ec48c3d4cf
oliolo <oliver_olofsson@hotmail.se>
2024-04-24 18:05:46 +0200

    Implement all router functions in university

--- Backend/nest-graphql-backend/src/routers/university/university.resolver.ts
+++ Backend/nest-graphql-backend/src/routers/university/university.resolver.ts
@@ -1,9 +1,43 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UniversityService } from './university.service';
+import { University } from 'src/entities';
+import { CreateUniversityInput } from 'src/inputTypes/create/create-university.input';
+import { RemoveUniversityInput } from 'src/inputTypes/remove/remove-university.input';
 
 @Resolver()
 export class UniversityResolver {
-  @Query(() => String)
-  sayHello(): string {
-    return 'Hello World!';
+  constructor(private universityService: UniversityService) {}
+
+  // Returns all university obejcts from database in a list.
+  @Query((returns) => [University])
+  async university(): Promise<University[]> {
+    return this.universityService.findAll();
+  }
+
+  // Finds a university from the database.
+  @Query((returns) => University)
+  async findOneUniversity(
+    @Args('findOneUniversity')
+    createUniversityInput: CreateUniversityInput,
+  ): Promise<University> {
+    return this.universityService.findOneUniversity(createUniversityInput);
+  }
+
+  // Creates a new university object for the database
+  @Mutation((returns) => University)
+  async createUniversity(
+    @Args('createUniversityInput')
+    createUniversityInput: CreateUniversityInput,
+  ): Promise<University> {
+    return this.universityService.createUniversity(createUniversityInput);
+  }
+
+  // Removes a university object for the database
+  @Mutation((returns) => University)
+  async removeUniversity(
+    @Args('removeUniversityInput')
+    removeUniversityInput: RemoveUniversityInput,
+  ): Promise<University> {
+    return this.universityService.removeUniversity(removeUniversityInput);
   }
 }

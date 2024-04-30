1baaea689f6d5de35d7210685f3e9c8f37ebd0e8
Erik <eribi813@student.liu.se>
2024-04-30 20:15:38 +0200

    "Minor change"

--- Frontend/masterfrontend/src/Screens/TeacherMasterProfilePage.tsx
+++ Frontend/masterfrontend/src/Screens/TeacherMasterProfilePage.tsx
@@ -237,18 +237,21 @@ const ProfileSection: React.FC<ProfileSectionProps> = ({ packages }) => {
           </>
         )}
       </div>
-      <button
-        onClick={handleEditProfileRequirements}
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ease-in-out"
-      >
-        Edit profile requirements
-      </button>
-      <ProfileRequirementsDialog
-        isOpen={isRequirementsDialogOpen}
-        closeDialog={closeRequirementsDialog}
-        saveRequirements={saveRequirements}
-        packages={packages}
-      />
+      <div className="p-4 border rounded-lg bg-teal-50 shadow flex flex-col justify-between h-auto">
+        <span>Profile information and restrictions</span>
+        <button
+          onClick={handleEditProfileRequirements}
+          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ease-in-out self-start mt-4"
+        >
+          Edit profile requirements
+        </button>
+        <ProfileRequirementsDialog
+          isOpen={isRequirementsDialogOpen}
+          closeDialog={closeRequirementsDialog}
+          saveRequirements={saveRequirements}
+          packages={packages}
+        />
+      </div>
     </div>
   );
 };

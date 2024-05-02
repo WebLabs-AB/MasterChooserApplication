e0a3c83dd46739e1b9d48c448732f01e7021d85d
oliolo <oliver_olofsson@hotmail.se>
2024-05-02 14:51:45 +0200

    Implement router functions for CourseStartingYear

--- Backend/nest-graphql-backend/src/inputTypes/remove/remove-course-startingYears.input.ts
+++ Backend/nest-graphql-backend/src/inputTypes/remove/remove-course-startingYears.input.ts
@@ -5,15 +5,12 @@ export class RemoveCourseStartingYearsInput {
   @Field(() => String, { description: 'course Id of the course' })
   courseId: string;
 
-  @Field((type) => Int, { description: 'year the course is being taught' })
-  yearTaught: number;
+  @Field((type) => Int, { description: 'starting year for the course' })
+  startYear: number;
 
-  @Field((type) => Int, { description: 'hp of the course' })
-  hp: number;
+  @Field((type) => Int, { description: 'level of the course' })
+  level: number;
 
-  @Field(() => String, { description: 'level of the course' })
-  level: string;
-
-  @Field(() => String, { description: 'schema block of the course' })
-  schemaBlock: string;
+  @Field((type) => Int, { description: 'schema block of the course' })
+  schemaBlock: number;
 }
--- Backend/nest-graphql-backend/src/routers/course-starting-year/course-starting-year.resolver.ts
+++ Backend/nest-graphql-backend/src/routers/course-starting-year/course-starting-year.resolver.ts
@@ -1,9 +1,46 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CourseStartingYearService } from './course-starting-year.service';
+import { CourseStartingYear } from 'src/entities';
+import { CreateCourseStartingYearsInput } from 'src/inputTypes/create/create-course-startingYears.input';
+import { RemoveCourseStartingYearsInput } from 'src/inputTypes/remove/remove-course-startingYears.input';
 
 @Resolver()
 export class CourseStartingYearResolver {
-  @Query(() => String)
-  sayHello(): string {
-    return 'Hello World!';
+  constructor(private courseStartingYearService: CourseStartingYearService) {}
+
+  // Returns all CourseStartingYear obejcts from the database in a list.
+  @Query((returns) => [CourseStartingYear])
+  async courseStartingYear(): Promise<CourseStartingYear[]> {
+    return this.courseStartingYearService.findAll();
+  }
+
+  // Creates a new CourseStartingYear object for the database.
+  @Mutation((returns) => CourseStartingYear)
+  async createCourseStartingYear(
+    @Args('createCourseStartingYearInput')
+    createCourseStartingYearsInput: CreateCourseStartingYearsInput,
+  ): Promise<CourseStartingYear> {
+    return this.courseStartingYearService.createCourseStartingYear(
+      createCourseStartingYearsInput,
+    );
+  }
+
+  // Removes a CourseStartingYear object from the database.
+  @Mutation((returns) => CourseStartingYear)
+  async removeCourseStartingYear(
+    @Args('removeCourseStartingYearsInput')
+    removeCourseStartingYearsInput: RemoveCourseStartingYearsInput,
+  ): Promise<CourseStartingYear> {
+    return this.courseStartingYearService.removeCourseStartingYear(
+      removeCourseStartingYearsInput,
+    );
+  }
+
+  // Return all courses that belong to a specific main area.
+  @Query((returns) => [CourseStartingYear])
+  async courseFromMainArea(
+    @Args('startYear') startYear: number,
+  ): Promise<CourseStartingYear[]> {
+    return this.courseStartingYearService.courseFromStartYear(startYear);
   }
 }

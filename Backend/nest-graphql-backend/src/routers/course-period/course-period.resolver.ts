08109e0d4347a4b7ea14333226d1c07d9eb3a721
oliolo <oliver_olofsson@hotmail.se>
2024-05-02 13:34:41 +0200

    Implement router functions in CoursePeriod

--- Backend/nest-graphql-backend/src/routers/course-period/course-period.resolver.ts
+++ Backend/nest-graphql-backend/src/routers/course-period/course-period.resolver.ts
@@ -1,9 +1,54 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CoursePeriodService } from './course-period.service';
+import { CoursePeriod } from 'src/entities';
+import { CreateCoursePeriodsInput } from 'src/inputTypes/create/create-course-periods.input';
+import { RemoveCoursePeriodsInput } from 'src/inputTypes/remove/remove-course-periods.input';
 
 @Resolver()
 export class CoursePeriodResolver {
-  @Query(() => String)
-  sayHello(): string {
-    return 'Hello World!';
+  constructor(private coursePeriodService: CoursePeriodService) {}
+
+  // Returns all CoursePeriod obejcts from the database in a list.
+  @Query((returns) => [CoursePeriod])
+  async coursePeriod(): Promise<CoursePeriod[]> {
+    return this.coursePeriodService.findAll();
+  }
+
+  // Creates a new CoursePeriod object for the database.
+  @Mutation((returns) => CoursePeriod)
+  async createCoursePeriod(
+    @Args('createCoursePeriodInput')
+    createCoursePeriodsInput: CreateCoursePeriodsInput,
+  ): Promise<CoursePeriod> {
+    return this.coursePeriodService.createCoursePeriod(
+      createCoursePeriodsInput,
+    );
+  }
+
+  // Removes a CoursePeriod object from the database.
+  @Mutation((returns) => CoursePeriod)
+  async removeCoursePeriod(
+    @Args('removeCoursePeriodInput')
+    removeCoursePeriodsInput: RemoveCoursePeriodsInput,
+  ): Promise<CoursePeriod> {
+    return this.coursePeriodService.removeCoursePeriod(
+      removeCoursePeriodsInput,
+    );
+  }
+
+  // Return all courses that belong to a specific period.
+  @Query((returns) => [CoursePeriod])
+  async periodFromCourse(
+    @Args('courseId') courseId: string,
+  ): Promise<CoursePeriod[]> {
+    return this.coursePeriodService.periodFromCourse(courseId);
+  }
+
+  // Return all periods that belong to a specific course.
+  @Query((returns) => [CoursePeriod])
+  async courseFromPeriod(
+    @Args('periodValue') periodValue: number,
+  ): Promise<CoursePeriod[]> {
+    return this.coursePeriodService.courseFromPeriod(periodValue);
   }
 }
